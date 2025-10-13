47f0773da9bd1e84623457c1de812e9bc3ab9cba
GabrielSiebra <gabrielsiebra2014@gmail.com>
2025-10-12 21:01:33 -0300

    Botom

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -4,6 +4,8 @@
  */
 package calculator;
 
+import calculator.visor.NewJFrame;
+
 /**
  *
  * @author Gabriel
@@ -14,7 +16,7 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(() -> new NewJFrame().setVisible(true));
     }
     
 }
